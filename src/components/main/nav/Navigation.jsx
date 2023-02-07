eed3817f7136b1a82df404dd0bcea118325288ab
KNCoder <itkncoder@gmail.com>
2023-02-07 11:04:47 +0500

    added preprice

--- src/components/main/nav/Navigation.jsx
+++ src/components/main/nav/Navigation.jsx
@@ -4,11 +4,11 @@ const Navigation = () => {
 	return (
 		<nav>
       <ul className="nav__ul flex justify-center items-center gap-14 mt-16 sm:mt-10 lg:flex">
-        <Link data-aos="fade-left" to="./info?name=Green&img=green&version=0&lists=38&words=990&per=66&audio=Good&narx=99 000" className="ease-linear duration-200 hover:bg-slate-800 text-lg p-2 cursor-pointer border border-solid border-slate-800 px-10 rounded-2xl"><li>Green</li></Link>
+        <Link data-aos="fade-left" to="./info?name=Green&img=green&version=0&lists=38&words=990&per=66&audio=Good&narx=99 000 / 55 000" className="ease-linear duration-200 hover:bg-slate-800 text-lg p-2 cursor-pointer border border-solid border-slate-800 px-10 rounded-2xl"><li>Green</li></Link>
 
-        <Link data-aos="fade-right" to="./info?name=Golden&img=gold&version=5&lists=38&words=990&per=33&audio=High&narx=129 000" className="ease-linear duration-200 hover:bg-slate-800 text-lg p-2 cursor-pointer border border-solid border-slate-800 px-10 rounded-2xl"><li>Golden</li></Link>
+        <Link data-aos="fade-right" to="./info?name=Golden&img=gold&version=5&lists=38&words=990&per=33&audio=High&narx=129 000 / 55 000" className="ease-linear duration-200 hover:bg-slate-800 text-lg p-2 cursor-pointer border border-solid border-slate-800 px-10 rounded-2xl"><li>Golden</li></Link>
 
-        <Link data-aos="fade-up" to="./info?name=Silver&img=silver&version=3&lists=44&words=1000&per=40&audio=High&narx=119 000" className="ease-linear duration-200 hover:bg-slate-800 text-lg p-2 cursor-pointer border border-solid border-slate-800 px-10 rounded-2xl"><li>Silver</li></Link>
+        <Link data-aos="fade-up" to="./info?name=Silver&img=silver&version=3&lists=44&words=1000&per=40&audio=High&narx=119 000 / 55 000" className="ease-linear duration-200 hover:bg-slate-800 text-lg p-2 cursor-pointer border border-solid border-slate-800 px-10 rounded-2xl"><li>Silver</li></Link>
       </ul>
     </nav>
 	)
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,9 +10,9 @@ const Main = () => {
 	return (
 		<div>
 			<div  data-aos="fade-top" className="blocks mt-10 flex justify-between items-center gap-24 p-10 sm:gap-4 sm:flex-col lg:gap-24 lg:flex-row">
-				<Book img={green} link="./info?name=Green&img=green&version=0&lists=38&words=990&per=66&audio=Good&narx=99 000" />
-				<Book img={gold} link="./info?name=Golden&img=gold&version=5&lists=38&words=990&per=33&audio=High&narx=129 000" />
-				<Book img={silver} link="./info?name=Silver&img=silver&version=3&lists=44&words=1000&per=40&audio=High&narx=119 000" />
+				<Book img={green} link="./info?name=Green&img=green&version=0&lists=38&words=990&per=66&audio=Good&narx=99 000 / 55 000" />
+				<Book img={gold} link="./info?name=Golden&img=gold&version=5&lists=38&words=990&per=33&audio=High&narx=129 000 / 55 000" />
+				<Book img={silver} link="./info?name=Silver&img=silver&version=3&lists=44&words=1000&per=40&audio=High&narx=119 000 / 55 000" />
 			</div>
 			<div className="audios flex justify-center items-center gap-20">
 				<div className="flex items-center justify-center flex-col">
